1f06719ab6e9bd50178c03bd396ecd466b7f6983
Kristian Losenara <whataboutkris@gmail.com>
2022-05-03 11:21:27 -0700

    starting stuff

--- Hike.h
+++ Hike.h
@@ -1,3 +1,14 @@
+/*
+    Team name
+    // blank line
+    Lastname, Firstname // member 1
+    Lastname, Firstname // member 2 and so on, if any
+    // blank line
+    Spring 2022
+    CS A250 - C++ 2
+    Project: Hiking in the US
+*/
+// blank line
 #include <iostream>
 #include <string>
 #include "Hike.cpp"
@@ -10,5 +21,6 @@ public:
 	string hikeName;
 	int durationHike;
 	char hikeDifficulty;
+private:
 
 };

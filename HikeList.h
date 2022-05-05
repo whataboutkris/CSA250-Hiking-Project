b0abee37689b7d821ac482d0f8c9c5721c5b5260
Amanda <AC350144@gmail.com>
2022-05-04 21:21:41 -0700

    nyoom

--- HikeList.h
+++ HikeList.h
@@ -1,1 +1,28 @@
-#pragma once
+/*
+    Team name
+    // blank line
+    Lastname, Firstname // member 1
+    Lastname, Firstname // member 2 and so on, if any
+    // blank line
+    Spring 2022
+    CS A250 - C++ 2
+    Project: Hiking in the US
+*/
+
+#ifndef HIKE_LIST_H
+#define HIKE_LIST_H
+
+#include <iostream>
+#include <string>
+#include <map>
+#include "Hike.h"
+
+class HikeList
+{
+public:
+
+private:
+    std::multimap<const Hike&, double> m;
+};
+
+#endif

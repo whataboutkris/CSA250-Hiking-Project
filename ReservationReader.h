14e07faf8eb031f12826ddb42e531fb44e824880
Kristian Losenara <whataboutkris@gmail.com>
2022-05-10 10:54:58 -0700

    try

---
+++ ReservationReader.h
@@ -0,0 +1,31 @@
+#pragma once
+/*
+	Cool Kids
+
+	Losenara, Kristian
+	Kelley, Bobby
+	Lu, Vince
+	Chen, Amanda
+
+	Spring 2022
+	CS A250 - C++ 2
+	Project: Hiking in the US
+*/
+
+#include "MemberList.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const string RESERVATION_FILE = "reservations_database.txt";
+
+//void createReservationList(ifstream& infile, ) {
+//	int ID = 0;
+//	string location;
+//	while (!infile.eof()) {
+//		
+//	}
+//}

b58531cd2b431eb8990f610a93fd53b374ca2abd
ac350144 <ac350144@gmail.com>
2022-05-10 10:59:43 -0700

    nyoom

--- ReservationReader.h
+++ ReservationReader.h
@@ -24,5 +24,29 @@ const string RESERVATIONS_FILE = "reservations_database.txt";
 
 void createReservations(ifstream& infile, Reservations& reservations);
 {
+	int memberID;
+	string hikeName;
 
+	while (!infile.eof())
+	{
+		infile >> memberID >> hikeName;
+		reservations.addReservation(memberID, hikeName);
+	}
+}
+
+void getReservationData(Reservations& reservations)
+{
+	ifstream infile;
+
+	infile.open(RESERVATIONS_FILE);
+
+	if (!infile)
+	{
+		cerr << RESERVATIONS_FILE << " does not exist." << endl;
+		exit(1);
+	}
+
+	createReservations(infile, reservations);
+
+	infile.close();
 }

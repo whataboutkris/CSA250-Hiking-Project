48d45954aaab5be87d22b9f27d028a20ab391bc0
ac350144 <ac350144@gmail.com>
2022-05-10 10:23:54 -0700

    nyoom

--- HikeReader.h
+++ HikeReader.h
@@ -1,5 +1,14 @@
 /*
-	(name header)
+	Cool Kids
+
+	Losenara, Kristian
+	Kelley, Bobby
+	Lu, Vince
+	Chen, Amanda
+
+	Spring 2022
+	CS A250 - C++ 2
+	Project: Hiking in the US
 */
 
 #include "HikeList.h"
---
+++ MemberReader.h
@@ -0,0 +1,28 @@
+/*
+    Cool Kids
+
+    Losenara, Kristian
+    Kelley, Bobby
+    Lu, Vince
+    Chen, Amanda
+
+    Spring 2022
+    CS A250 - C++ 2
+    Project: Hiking in the US
+*/
+
+#include "MemberList.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const string MEMBERS_FILE = "members_database.txt";
+
+void createMemberList()
+{
+
+}

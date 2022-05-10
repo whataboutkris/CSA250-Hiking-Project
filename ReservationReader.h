0295b1172c3a969d38da55540da6c46f435c4efa
ac350144 <ac350144@gmail.com>
2022-05-10 10:51:44 -0700

    nyoom

---
+++ ReservationReader.h
@@ -0,0 +1,28 @@
+/*
+	Cool Kids
+
+	Losenara, Kristian
+	Kelley, Bobby
+	Lu, Vince
+	Chen, Amanda
+
+	Spring 2022
+	CS A250 - C++ 2
+	Project: Hiking in the US
+*/
+
+#include "Reservations.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const string RESERVATIONS_FILE = "reservations_database.txt";
+
+void createReservations(ifstream& infile, Reservations& reservations);
+{
+
+}

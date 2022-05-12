5772c4326fee5e590b2c70d4882cbf0b4555146a
vlu8 <70289579+vlu8@users.noreply.github.com>
2022-05-12 01:20:54 -0700

    Update Reservations.cpp

--- Reservations.cpp
+++ Reservations.cpp
@@ -6,27 +6,30 @@ using namespace std;
 
 void Reservations::clearList()
 {
-    Node* temp = first;
-    while (first != nullptr)
+    if (count == 1)
     {
-        first = first->getNext();
-        delete temp;
-        temp = first;
+        delete first;
+        first = last = nullptr;
+        count = 0;
     }
-    last = nullptr;
-    count = 0;
+    else if (count > 1)
+    {
+        Node* current = first;
+        while (first != nullptr)
+        {
+            first = first->getNext();
+            delete current;
+            current = first;
+        }
+        
+        last = nullptr;
+        count = 0;
+    }
 }
 
 Reservations::~Reservations()
 {
-    Node *current = first;
-    while(current != nullptr)
-    {
-        Node *temp = current->getNext();
-        delete current;
-        current = temp;
-    }
-    first = last = nullptr;
+    clearList();
 }
 =======
 #include <iostream>
